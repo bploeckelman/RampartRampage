47f1a8efc702968f177b992b6b5166041e0d3e8d
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2014-09-01 14:17:44 -0500

    Project setup

---
+++ desktop/src/com/lando/systems/rampartrampage/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.lando.systems.rampartrampage.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.lando.systems.rampartrampage.RampartRampage;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RampartRampage(), config);
+	}
+}
---
+++ html/src/com/lando/systems/rampartrampage/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.lando.systems.rampartrampage.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.lando.systems.rampartrampage.RampartRampage;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new RampartRampage();
+        }
+}
