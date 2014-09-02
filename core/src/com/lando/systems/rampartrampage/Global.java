30d713445af9fcdce1b9f42a1269f0c3bbc66e82
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2014-09-02 00:24:12 -0500

    add tween manager and some standard accessors

---
+++ core/src/com/lando/systems/rampartrampage/Global.java
@@ -0,0 +1,10 @@
+package com.lando.systems.rampartrampage;
+
+import aurelienribon.tweenengine.TweenManager;
+
+/**
+ * Brian Ploeckelman created on 9/1/2014.
+ */
+public class Global {
+    public static final TweenManager tweenManager = new TweenManager();
+}
