a6b802498d60b8617a11a3c09940bcb74b2b978e
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2014-09-08 22:58:02 -0500

    refactor extract geometry from gamestate to scene

--- core/src/com/lando/systems/rampartrampage/GameState.java
+++ core/src/com/lando/systems/rampartrampage/GameState.java
@@ -1,7 +1,6 @@
 package com.lando.systems.rampartrampage;
 
 import aurelienribon.tweenengine.*;
-import aurelienribon.tweenengine.equations.*;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.Input.Keys;
@@ -9,44 +8,23 @@ import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g3d.*;
-import com.badlogic.gdx.graphics.g3d.attributes.BlendingAttribute;
-import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
-import com.badlogic.gdx.graphics.g3d.attributes.IntAttribute;
-import com.badlogic.gdx.graphics.g3d.attributes.TextureAttribute;
-import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
-import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
-import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
-import com.badlogic.gdx.math.Quaternion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Disposable;
 import com.lando.systems.rampartrampage.accessors.ColorAccessor;
 import com.lando.systems.rampartrampage.accessors.Vector2Accessor;
 import com.lando.systems.rampartrampage.accessors.Vector3Accessor;
 import com.lando.systems.rampartrampage.scene.Scene;
-import com.lando.systems.rampartrampage.util.DebugCamera;
 import com.lando.systems.rampartrampage.util.InputHandler;
 
 /**
  * Brian Ploeckelman created on 9/1/2014.
  */
 public class GameState implements Disposable {
-//    final Color bg = new Color(0, 0.75f, 1, 1);
-    final Color bg = new Color(0.3f, 0.3f, 0.3f, 1);
+    public final Color bg = new Color(0.3f, 0.3f, 0.3f, 1);
 
-    public SpriteBatch batch;
+    public SpriteBatch spriteBatch;
     public ModelBatch modelBatch;
-
-    public Texture texture;
-
-    public Environment env;
-    public Model axes;
-    public Model box;
-    public Model ground;
-    public Model selectorModel;
-    public ModelInstance selectorInstance;
-    public Array<ModelInstance> modelInstances;
 
     public Scene scene;
 
@@ -59,22 +37,17 @@ public class GameState implements Disposable {
         Tween.registerAccessor(Vector2.class, new Vector2Accessor());
         Tween.registerAccessor(Vector3.class, new Vector3Accessor());
 
-        batch = new SpriteBatch();
+        spriteBatch = new SpriteBatch();
         modelBatch = new ModelBatch();
 
-        initializeScene();
+        scene = new Scene(this);
+
         initializeInput();
     }
 
     public void update(float delta) {
         Global.tweenManager.update(delta);
-
         inputHandler.handleInput(delta);
-
-        selectorInstance.nodes.get(0).translation.set(inputHandler.mouse_world);
-        selectorInstance.nodes.get(0).translation.y = 0;
-        selectorInstance.calculateTransforms();
-
         scene.update(delta);
     }
 
@@ -82,79 +55,14 @@ public class GameState implements Disposable {
         Gdx.gl.glClearColor(bg.r, bg.g, bg.b, bg.a);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
-        modelBatch.begin(scene.currentCamera);
-        modelBatch.render(modelInstances, env);
-        modelBatch.render(selectorInstance, env);
-
-        scene.camera1.render(modelBatch);
-        scene.camera2.render(modelBatch);
-        scene.camera3.render(modelBatch);
-        modelBatch.end();
+        scene.render(modelBatch, spriteBatch);
     }
 
     @Override
     public void dispose() {
         scene.dispose();
-        box.dispose();
-        axes.dispose();
-        ground.dispose();
-        selectorModel.dispose();
-        batch.dispose();
+        spriteBatch.dispose();
         modelBatch.dispose();
-    }
-
-    private void initializeScene() {
-        scene = new Scene();
-
-        final ModelBuilder modelBuilder = new ModelBuilder();
-
-        final float axisLength = 10;
-        final Material axisMaterial = new Material(ColorAttribute.createDiffuse(Color.WHITE));
-        final long axisAttrs = VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal;
-        axes = modelBuilder.createXYZCoordinates(axisLength, axisMaterial, axisAttrs);
-
-        texture = new Texture("badlogic.jpg");
-        final float boxSize = 1;
-        final Material boxMaterial = new Material(TextureAttribute.createDiffuse(texture));
-        final long boxAttrs = VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal | VertexAttributes.Usage.TextureCoordinates;
-        box = modelBuilder.createBox(boxSize, boxSize, boxSize, boxMaterial, boxAttrs);
-
-        final int xDivisions = 100;
-        final int zDivisions = 100;
-        final float xSize = 1;
-        final float zSize = 1;
-        final Material groundMaterial = new Material(ColorAttribute.createDiffuse(bg.cpy()));
-        final long groundAttrs = VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal | VertexAttributes.Usage.Color;
-        ground = modelBuilder.createLineGrid(xDivisions, zDivisions, xSize, zSize, groundMaterial, groundAttrs);
-
-        final Material selectorMaterial = new Material(
-                ColorAttribute.createDiffuse(1, 1, 1, 0.7f),
-                TextureAttribute.createDiffuse(texture),
-                IntAttribute.createCullFace(GL20.GL_NONE));//,
-//                new BlendingAttribute(true, 0.7f));
-        final long selectorAttrs = VertexAttributes.Usage.Position
-//                                 | VertexAttributes.Usage.Normal
-                                 | VertexAttributes.Usage.TextureCoordinates
-                                 | VertexAttributes.Usage.Color;
-        selectorModel = modelBuilder.createRect(
-                0, 0, 0,
-                1, 0, 0,
-                1, 0, 1,
-                0, 0, 1,
-                0, 1, 0,
-                selectorMaterial,
-                selectorAttrs);
-        selectorInstance = new ModelInstance(selectorModel);
-
-        modelInstances = new Array<ModelInstance>();
-        modelInstances.add(new ModelInstance(axes));
-        modelInstances.add(new ModelInstance(box));
-        modelInstances.add(new ModelInstance(ground));
-
-        env = new Environment();
-        env.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1));
-        env.add(new DirectionalLight().set(Color.PURPLE, -5, 5, 5));
-        // ...
     }
 
     private void initializeInput() {
@@ -167,7 +75,7 @@ public class GameState implements Disposable {
                     case Keys.ESCAPE: Gdx.app.exit(); break;
                     case Keys.TAB: scene.switchCameras(); break;
                     case Keys.SPACE:
-                        spawnBox(new Vector3(inputHandler.mouse_world.x, 0, inputHandler.mouse_world.z));
+                        scene.spawnBox(new Vector3(inputHandler.mouse_world.x, 0, inputHandler.mouse_world.z));
                         break;
                 }
                 return false;
@@ -191,25 +99,4 @@ public class GameState implements Disposable {
         Gdx.input.setInputProcessor(inputMux);
     }
 
-    final Quaternion unit_quaternion = new Quaternion();
-    final Vector3 unit_vector3 = new Vector3(1,1,1);
-    // TODO :
-    private void spawnBox(Vector3 position) {
-        this.spawnBox(position, unit_vector3);
-    }
-
-    private void spawnBox(Vector3 position, Vector3 scale) {
-        this.spawnBox(position, scale, unit_quaternion);
-    }
-
-    private void spawnBox(Vector3 position, Vector3 scale, Quaternion rotation) {
-        Gdx.app.log("SPAWN_BOX", "pos: " + position.toString() + ", scale: " + scale.toString() + ", rot: " + rotation.toString());
-
-        ModelInstance boxInstance = new ModelInstance(box);
-        boxInstance.nodes.get(0).translation.set(position);
-        boxInstance.nodes.get(0).scale.set(scale);
-        boxInstance.nodes.get(0).rotation.set(rotation);
-        boxInstance.calculateTransforms();
-        modelInstances.add(boxInstance);
-    }
 }
--- core/src/com/lando/systems/rampartrampage/scene/Scene.java
+++ core/src/com/lando/systems/rampartrampage/scene/Scene.java
@@ -2,11 +2,22 @@ package com.lando.systems.rampartrampage.scene;
 
 import aurelienribon.tweenengine.*;
 import aurelienribon.tweenengine.equations.Back;
-import com.badlogic.gdx.graphics.Camera;
-import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.*;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.attributes.IntAttribute;
+import com.badlogic.gdx.graphics.g3d.attributes.TextureAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.math.Quaternion;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Disposable;
 import com.lando.systems.rampartrampage.Const;
+import com.lando.systems.rampartrampage.GameState;
 import com.lando.systems.rampartrampage.Global;
 import com.lando.systems.rampartrampage.accessors.Vector3Accessor;
 import com.lando.systems.rampartrampage.util.DebugCamera;
@@ -15,6 +26,10 @@ import com.lando.systems.rampartrampage.util.DebugCamera;
  * Brian Ploeckelman created on 9/7/2014.
  */
 public class Scene implements Disposable {
+
+    private final GameState gameState;
+
+    public Texture texture;
 
     public Camera currentCamera;
     public Camera transitionCamera;
@@ -25,44 +40,20 @@ public class Scene implements Disposable {
 
     public CameraInputController camController;
 
-    public Scene() {
-        final float frustum_alpha = 0.6f;
+    public Environment env;
+    public Model axes;
+    public Model box;
+    public Model ground;
+    public Model selectorModel;
+    public ModelInstance selectorInstance;
+    public Array<ModelInstance> modelInstances;
 
-        camera1 = new DebugCamera(Const.default_fov, Const.viewport_width, Const.viewport_height);
-        camera1.position.set(10, 2, 0);
-        camera1.lookAt(0, 0, 0);
-        camera1.near = 1;
-        camera1.far = 25;
-        camera1.update();
-        camera1.setFrustumColor(new Color(1, 0, 0, frustum_alpha));
-        camera1.updateFrustumModel();
 
-        camera2 = new DebugCamera(Const.default_fov, Const.viewport_width, Const.viewport_height);
-        camera2.position.set(10, 1, 10);
-        camera2.lookAt(0, 0, 0);
-        camera2.near = 1;
-        camera2.far = 50;
-        camera2.update();
-        camera2.setFrustumColor(new Color(0, 1, 0, frustum_alpha));
-        camera2.updateFrustumModel();
+    public Scene(GameState gameState) {
+        this.gameState = gameState;
 
-        camera3 = new DebugCamera(Const.default_fov, Const.viewport_width, Const.viewport_height);
-        camera3.position.set(0, 3, 10);
-        camera3.lookAt(0, 0, 0);
-        camera3.near = 1;
-        camera3.far = 75;
-        camera3.update();
-        camera3.setFrustumColor(new Color(0, 0, 1, frustum_alpha));
-        camera3.updateFrustumModel();
-
-        transitionCamera = new DebugCamera(Const.default_fov, Const.viewport_width, Const.viewport_height);
-        transitionCamera.near = 1;
-        transitionCamera.far = 1000;
-        transitionCamera.update();
-
-        currentCamera = camera1;
-
-        camController = new CameraInputController(camera1);
+        initializeCameras();
+        initializeGeometry();
     }
 
     public void update(float delta) {
@@ -77,17 +68,37 @@ public class Scene implements Disposable {
         camera3.updateFrustumModel();
 
         transitionCamera.update(true);
+
+        selectorInstance.nodes.get(0).translation.set(gameState.inputHandler.mouse_world);
+        selectorInstance.nodes.get(0).translation.y = 0;
+        selectorInstance.calculateTransforms();
+    }
+
+    public void render(ModelBatch modelBatch, SpriteBatch spriteBatch) {
+        modelBatch.begin(currentCamera);
+        {
+            modelBatch.render(modelInstances, env);
+            modelBatch.render(selectorInstance, env);
+
+            camera1.render(modelBatch);
+            camera2.render(modelBatch);
+            camera3.render(modelBatch);
+        }
+        modelBatch.end();
     }
 
     @Override
     public void dispose() {
+        box.dispose();
+        axes.dispose();
+        ground.dispose();
+        selectorModel.dispose();
         camera3.dispose();
         camera2.dispose();
         camera1.dispose();
     }
 
     public void switchCameras() {
-
         currentCamera.far = 100;
         transitionCamera.position.set(currentCamera.position);
         transitionCamera.direction.set(currentCamera.direction);
@@ -162,6 +173,118 @@ public class Scene implements Disposable {
         currentCamera.far = 1000;
     }
 
+    private void initializeCameras() {
+        final float frustum_alpha = 0.6f;
 
+        camera1 = new DebugCamera(Const.default_fov, Const.viewport_width, Const.viewport_height);
+        camera1.position.set(10, 2, 0);
+        camera1.lookAt(0, 0, 0);
+        camera1.near = 1;
+        camera1.far = 25;
+        camera1.update();
+        camera1.setFrustumColor(new Color(1, 0, 0, frustum_alpha));
+        camera1.updateFrustumModel();
+
+        camera2 = new DebugCamera(Const.default_fov, Const.viewport_width, Const.viewport_height);
+        camera2.position.set(10, 1, 10);
+        camera2.lookAt(0, 0, 0);
+        camera2.near = 1;
+        camera2.far = 50;
+        camera2.update();
+        camera2.setFrustumColor(new Color(0, 1, 0, frustum_alpha));
+        camera2.updateFrustumModel();
+
+        camera3 = new DebugCamera(Const.default_fov, Const.viewport_width, Const.viewport_height);
+        camera3.position.set(0, 3, 10);
+        camera3.lookAt(0, 0, 0);
+        camera3.near = 1;
+        camera3.far = 75;
+        camera3.update();
+        camera3.setFrustumColor(new Color(0, 0, 1, frustum_alpha));
+        camera3.updateFrustumModel();
+
+        transitionCamera = new DebugCamera(Const.default_fov, Const.viewport_width, Const.viewport_height);
+        transitionCamera.near = 1;
+        transitionCamera.far = 1000;
+        transitionCamera.update();
+
+        currentCamera = camera1;
+
+        camController = new CameraInputController(camera1);
+    }
+
+    private void initializeGeometry() {
+        final ModelBuilder modelBuilder = new ModelBuilder();
+
+        final float axisLength = 10;
+        final Material axisMaterial = new Material(ColorAttribute.createDiffuse(Color.WHITE));
+        final long axisAttrs = VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal;
+        axes = modelBuilder.createXYZCoordinates(axisLength, axisMaterial, axisAttrs);
+
+        texture = new Texture("badlogic.jpg");
+        final float boxSize = 1;
+        final Material boxMaterial = new Material(TextureAttribute.createDiffuse(texture));
+        final long boxAttrs = VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal | VertexAttributes.Usage.TextureCoordinates;
+        box = modelBuilder.createBox(boxSize, boxSize, boxSize, boxMaterial, boxAttrs);
+
+        final int xDivisions = 100;
+        final int zDivisions = 100;
+        final float xSize = 1;
+        final float zSize = 1;
+        final Material groundMaterial = new Material(ColorAttribute.createDiffuse(gameState.bg.cpy()));
+        final long groundAttrs = VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal | VertexAttributes.Usage.Color;
+        ground = modelBuilder.createLineGrid(xDivisions, zDivisions, xSize, zSize, groundMaterial, groundAttrs);
+
+        final Material selectorMaterial = new Material(
+                ColorAttribute.createDiffuse(1, 1, 1, 0.7f),
+                TextureAttribute.createDiffuse(texture),
+                IntAttribute.createCullFace(GL20.GL_NONE));//,
+//                new BlendingAttribute(true, 0.7f));
+        final long selectorAttrs = VertexAttributes.Usage.Position
+//                                 | VertexAttributes.Usage.Normal
+                | VertexAttributes.Usage.TextureCoordinates
+                | VertexAttributes.Usage.Color;
+        selectorModel = modelBuilder.createRect(
+                0, 0, 0,
+                1, 0, 0,
+                1, 0, 1,
+                0, 0, 1,
+                0, 1, 0,
+                selectorMaterial,
+                selectorAttrs);
+        selectorInstance = new ModelInstance(selectorModel);
+
+        modelInstances = new Array<ModelInstance>();
+        modelInstances.add(new ModelInstance(axes));
+        modelInstances.add(new ModelInstance(box));
+        modelInstances.add(new ModelInstance(ground));
+
+        env = new Environment();
+        env.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1));
+        env.add(new DirectionalLight().set(Color.PURPLE, -5, 5, 5));
+        // ...
+    }
+
+    final Quaternion unit_quaternion = new Quaternion();
+    final Vector3 unit_vector3 = new Vector3(1,1,1);
+    // TODO :
+    public void spawnBox(Vector3 position) {
+        this.spawnBox(position, unit_vector3);
+    }
+
+    public void spawnBox(Vector3 position, Vector3 scale) {
+        this.spawnBox(position, scale, unit_quaternion);
+    }
+
+    public void spawnBox(Vector3 position, Vector3 scale, Quaternion rotation) {
+        Gdx.app.log("SPAWN_BOX", "pos: " + position.toString() + ", scale: " + scale.toString() + ", rot: " + rotation.toString());
+
+        ModelInstance boxInstance = new ModelInstance(box);
+        boxInstance.nodes.get(0).translation.set(position);
+        boxInstance.nodes.get(0).scale.set(scale);
+        boxInstance.nodes.get(0).rotation.set(rotation);
+        boxInstance.calculateTransforms();
+        modelInstances.add(boxInstance);
+    }
 
 }
